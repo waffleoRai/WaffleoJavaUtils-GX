88279640bdc0343d2d77d0b739d536482ec95044
Blythe <bghospe@yahoo.com>
2024-01-21 11:22:41 -0600

    Debug off for LZMu, qximage field add

--- src/waffleoRai_Compression/lz77/LZMu.java
+++ src/waffleoRai_Compression/lz77/LZMu.java
@@ -70,7 +70,7 @@ public class LZMu {
 	
 	/*--- Constants ---*/
 	
-	private static final boolean DEBUG_ON = true;
+	private static final boolean DEBUG_ON = false;
 	
 	public static final int BACK_WINDOW_SIZE = 0x2000; //Treated as unsigned 13 bit
 	//public static final int BACK_WINDOW_SIZE = 0x1000; //If treated as signed 13 bit, then -4096 is most negative value
